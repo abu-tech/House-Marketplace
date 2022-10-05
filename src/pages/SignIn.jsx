ee55390f02f0e8284297cc37ff960f1a9a7c46ec
Mohammed samir <abutech8801@gmail.com>
2022-10-05 11:58:15 +0530

    Some finishing

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -41,7 +41,7 @@ function SignIn() {
 <div className="hero min-h-screen bg-base-200">
   <div className="hero-content flex-col lg:flex-row-reverse">
     <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">Welcome Back!</h1>
+      <h1 className="font-bold xl:text-5xl sm:text-3xl">Welcome Back!</h1>
       <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
     </div>
     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
