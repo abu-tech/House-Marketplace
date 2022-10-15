794047e9b8f049150ad4133522f52d9b4da46869
Mohammed samir <abutech8801@gmail.com>
2022-10-15 13:46:32 +0530

    map added

---
+++ src/components/Mapbox.jsx
@@ -0,0 +1,22 @@
+import Map, {Marker, Popup}  from 'react-map-gl';
+
+function Mapbox({lng, lat}) {
+  return (
+    <Map
+        initialViewState={{
+        longitude: lng,
+        latitude: lat,
+        zoom: 8
+        }}
+        style={{width: 700, height: 300}}
+        mapboxAccessToken={process.env.REACT_APP_MAP_API_KEY}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        projection= 'globe'
+        >
+        <Marker longitude={lng} latitude={lat} anchor="bottom" color='black'>
+        </Marker>
+    </Map>
+  )
+}
+
+export default Mapbox
--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -11,6 +11,7 @@ import {MdBathtub, MdLocalParking} from 'react-icons/md'
 import {GiSofa} from 'react-icons/gi'
 import {BsPeopleFill, BsShareFill} from 'react-icons/bs'
 import {toast} from 'react-toastify'
+import Mapbox from '../components/Mapbox'
 
 function Listing() {
     const [listing, setListing] = useState(null);
@@ -48,7 +49,7 @@ function Listing() {
           <div className='flex justify-between'><h1 className='font-bold text-md sm:text-2xl '>{listing.name}</h1><button type='button' className="btn btn-circle btn-outline" onClick={handleShare}><BsShareFill /></button>
           </div>
           <div className='flex flex-end mb-5'>
-          <span className="badge mr-2 text-xs sm:text-sm"><FaRupeeSign/>{listing.offer ? listing.regularPrice-listing.discountedPrice : 'No'} Discount</span>
+          <span className="badge mr-2 text-white text-xs sm:text-sm"><FaRupeeSign/>{listing.offer ? listing.regularPrice-listing.discountedPrice : 'No'} Discount</span>
           <span className="badge badge-accent mx-2 text-xs sm:text-sm">For {listing.type[0].toUpperCase()+listing.type.slice(1)}</span>
           </div>
           <span className="flex justify-start font-semibold"><IoLocationSharp /><p className='mx-2'>{listing.location}</p></span>
@@ -68,8 +69,7 @@ function Listing() {
         </div>
       </div>
       <div>
-        {/* map */}
-        map and images
+        <Mapbox lat={listing.geolocation.lat} lng={listing.geolocation.lng} />
       </div>
     </div>
   )
